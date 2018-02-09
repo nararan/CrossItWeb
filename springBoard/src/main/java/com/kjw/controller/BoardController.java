300ed77910a41c89a8496a04037187cbe66beb08
201511006 <wndnjs3610@naver.com>
2018-02-09 14:55:13 +0900

    전체 목록/조회 구현

--- springBoard/src/main/java/com/kjw/controller/BoardController.java
+++ springBoard/src/main/java/com/kjw/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kjw.domain.BoardVO;
@@ -46,10 +47,18 @@ public class BoardController {
 	}
 	
 	
-	
 	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
 	public void listAll(Model model)throws Exception{
 		
 		logger.info("show all list......");
+	
+		model.addAttribute("list", service.listAll());
+	}
+	
+	
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, Model model)throws Exception{
+		
+		model.addAttribute(service.read(bno)); //BoardVO클래스의 객체임으로 boardVO로 저장됨!
 	}
 }
--- springBoard/src/main/webapp/WEB-INF/views/include/header.jsp
+++ springBoard/src/main/webapp/WEB-INF/views/include/header.jsp
@@ -39,7 +39,7 @@
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>A</b>LT</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Zerock</b> PROJECT</span>
+          <span class="logo-lg"><b>KJW</b> PROJECT</span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
